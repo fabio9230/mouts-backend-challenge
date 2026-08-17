5e21bf0127d6a0d32e4928fd15eec65753217caa
Fabio França <fabio9230@hotmail.com>
2026-08-17 00:22:50 -0300

    feature: add sales cancel item

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CancelItem/CancelItemHandler.cs
@@ -0,0 +1,49 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Common;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CancelItem;
+
+public sealed class CancelItemHandler(
+    ISaleRepository repository,
+    IMapper mapper,
+    ISaleEventPublisher eventPublisher,
+    ILogger<CancelItemHandler> logger)
+    : IRequestHandler<CancelItemCommand, SaleModel>
+{
+    public async Task<SaleModel> Handle(
+        CancelItemCommand request,
+        CancellationToken cancellationToken)
+    {
+        logger
+            .LogInformation(
+            "Cancelling sale item. SaleId={SaleId}, ItemId={ItemId}",
+            request.SaleId,
+            request.ItemId);
+
+        var sale = await repository
+            .GetByIdAsync(request.SaleId, cancellationToken)
+                ?? throw new KeyNotFoundException($"Sale with ID {request.SaleId} not found.");
+
+        if (sale.Status == Domain.Enums.SaleStatus.Cancelled)
+            throw new InvalidOperationException("A cancelled sale cannot have its items modified.");
+
+        sale.CancelItem(request.ItemId);
+        await repository.UpdateAsync(sale, cancellationToken);
+
+        foreach (var @event in sale.Events)
+            await eventPublisher.PublishAsync(@event, cancellationToken);
+
+        logger
+            .LogInformation(
+                "Sale item cancelled successfully. SaleId={SaleId}, ItemId={ItemId}",
+                sale.Id,
+                request.ItemId);
+
+        sale.ClearEvents();
+
+        return mapper.Map<SaleModel>(sale);
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CancelItem/CancelItemValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CancelItem;
+
+public sealed class CancelItemValidator : AbstractValidator<CancelItemCommand>
+{
+    public CancelItemValidator()
+    {
+        RuleFor(x => x.SaleId)
+            .NotEmpty();
+
+        RuleFor(x => x.ItemId)
+            .NotEmpty();
+    }
+}
