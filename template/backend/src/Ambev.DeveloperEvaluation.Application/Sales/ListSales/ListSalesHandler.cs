2f196db411e2bb27efeb16658887037c7eaad097
Fabio França <fabio9230@hotmail.com>
2026-08-17 00:24:16 -0300

    feature: add sales list

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/ListSales/ListSalesHandler.cs
@@ -0,0 +1,27 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.Common;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.ListSales;
+
+public sealed class ListSalesHandler(
+    ISaleRepository repository,
+    IMapper mapper,
+    ILogger<ListSalesHandler> logger)
+    : IRequestHandler<ListSalesCommand, IReadOnlyCollection<SaleModel>>
+{
+    public async Task<IReadOnlyCollection<SaleModel>> Handle(
+        ListSalesCommand request,
+        CancellationToken cancellationToken)
+    {
+        logger
+            .LogInformation("Listing sales.");
+
+        var sales = await repository
+            .GetAllAsync(cancellationToken);
+
+        return mapper.Map<IReadOnlyCollection<SaleModel>>(sales);
+    }
+}
