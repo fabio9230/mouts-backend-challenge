c42f07bb15bef2b48cd034a1006268489070041b
Fabio França <fabio9230@hotmail.com>
2026-08-17 00:23:51 -0300

    feature: add sales delete item

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleHandler.cs
@@ -0,0 +1,29 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+public sealed class DeleteSaleHandler(
+    ISaleRepository repository,
+    ILogger<DeleteSaleHandler> logger)
+    : IRequestHandler<DeleteSaleCommand>
+{
+    public async Task Handle(
+        DeleteSaleCommand request,
+        CancellationToken cancellationToken)
+    {
+        logger
+            .LogInformation(
+                "Deleting sale. SaleId={SaleId}",
+                request.Id);
+
+        if (!await repository.DeleteAsync(request.Id, cancellationToken))
+            throw new KeyNotFoundException($"Sale with ID {request.Id} not found.");
+
+        logger
+            .LogInformation(
+                "Sale deleted successfully. SaleId={SaleId}",
+                request.Id);
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+public sealed class DeleteSaleValidator : AbstractValidator<DeleteSaleCommand>
+{
+    public DeleteSaleValidator() => RuleFor(x => x.Id).NotEmpty();
+}
